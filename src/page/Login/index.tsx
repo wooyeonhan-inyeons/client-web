b5394db5b7204b5505e0881b7273e06c704a8e9a
3o14 <dldnjswn4265@gmail.com>
2023-04-04 20:59:49 +0900

    feat #4 : 시작 페이지 슬라이드 뷰 구현

--- src/page/Login/index.tsx
+++ src/page/Login/index.tsx
@@ -3,11 +3,13 @@ import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import MobileStepper from "@mui/material/MobileStepper";
 import SwipeableViews from "react-swipeable-views";
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, CssBaseline, Grid, Typography } from "@mui/material";
 import SaveBtn from "../Category/components/SaveBtn";
-import loginImg1 from "./img/loginImg1.png";
-import loginImg2 from "./img/loginImg1.png";
-import loginImg3 from "./img/loginImg1.png";
+import { Global } from "@emotion/react";
+
+// import loginImg1 from "./img/loginImg1.png";
+// import loginImg2 from "./img/loginImg1.png";
+// import loginImg3 from "./img/loginImg1.png";
 
 const images = [
   {
@@ -26,15 +28,18 @@ const images = [
 
 const introduction = [
   {
-    mainText: "레이더를 통해 내 주변의 우연을 찾기",
+    mainTextLine1: "레이더를 통해",
+    mainTextLine2: "내 주변의 우연을 찾기",
     subText: "원하는 카테고리와 범위 설정하기!",
   },
   {
-    mainText: "발견한 우연을 확인하고 댓글 입력하기",
+    mainTextLine1: "발견한 우연을",
+    mainTextLine2: "확인하고 댓글 입력하기",
     subText: "소통과 재미를 동시에!",
   },
   {
-    mainText: "지도에서 발견한 우연을 볼 수 있어요",
+    mainTextLine1: "지도에서 발견한 우연을",
+    mainTextLine2: "볼 수 있어요",
     subText: "발견했던 우연을 확인 가능!",
   },
 ];
@@ -50,6 +55,14 @@ function SwipeableTextMobileStepper() {
 
   return (
     <Grid container alignItems="center" justifyContent="center">
+      <CssBaseline />
+      <Global
+        styles={{
+          body: {
+            backgroundColor: "#EFF0F2",
+          },
+        }}
+      />
       {/* 이미지 영역(상단) */}
       <Grid item>
         <SwipeableViews
@@ -67,8 +80,8 @@ function SwipeableTextMobileStepper() {
                     display: "block",
                     overflow: "hidden",
                     width: "100%",
-                    position: "relative",
-                    bottom: "3rem",
+                    pt: "5rem",
+                    pb: "5rem",
                   }}
                   src={step.imgPath}
                 />
@@ -78,48 +91,58 @@ function SwipeableTextMobileStepper() {
         </SwipeableViews>
       </Grid>
 
-      {/* 스와이프 Dot (•••) */}
-      <Grid item>
-        <MobileStepper
-          steps={maxSteps}
-          position="static"
-          activeStep={activeStep}
-          backButton={null}
-          nextButton={null}
-          sx={{
-            alignItems: "center",
-            justifyContent: "center",
-            dotActiveStyle: { backgroundColor: "#1976d2" },
-            dotStyle: { mx: 1 }, // dot 간격 조정하기
-          }}
-        />
-      </Grid>
+      <Grid item sx={{ backgroundColor: "white", pt: "1rem" }}>
+        {/* 스와이프 Dot (•••) */}
+        <Grid item>
+          <MobileStepper
+            steps={maxSteps}
+            position="static"
+            activeStep={activeStep}
+            backButton={null}
+            nextButton={null}
+            sx={{
+              alignItems: "center",
+              justifyContent: "center",
+              dotActiveStyle: { backgroundColor: "#1976d2" },
+              dotStyle: { mx: 1 }, // dot 간격 조정하기
+            }}
+          />
+        </Grid>
 
-      {/* 텍스트 영역(하단) */}
-      <Grid>
-        <SwipeableViews
-          axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-          index={activeStep}
-          onChangeIndex={handleStepChange}
-          enableMouseEvents
+        {/* 텍스트 영역(하단) */}
+        <Grid
+          container
+          sx={{ p: "1rem" }}
+          alignItems="center"
+          justifyContent="center"
         >
-          {introduction.map((text, index) => (
-            <Container sx={{ pt: "3rem", pb: "3rem" }} key={index}>
-              <Typography variant="h5" sx={{ fontWeight: 600 }}>
-                {text.mainText}
-              </Typography>
-              <Typography
-                variant="subtitle1"
-                sx={{ color: "#A2A2A2", pt: "0.7rem" }}
-              >
-                {text.subText}
-              </Typography>
-            </Container>
-          ))}
-        </SwipeableViews>
-      </Grid>
-      <Grid item>
-        <SaveBtn text="시작하기" />
+          <Grid item sx={{ p: "1rem" }}>
+            <SwipeableViews
+              axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+              index={activeStep}
+              onChangeIndex={handleStepChange}
+              enableMouseEvents
+            >
+              {introduction.map((text, index) => (
+                <Grid item sx={{ pt: "2rem", pb: "2rem" }} key={index}>
+                  <Typography variant="h5" sx={{ fontWeight: 600 }}>
+                    {text.mainTextLine1} <br />
+                    {text.mainTextLine2}
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    sx={{ color: "#A2A2A2", pt: "0.7rem" }}
+                  >
+                    {text.subText}
+                  </Typography>
+                </Grid>
+              ))}
+            </SwipeableViews>
+          </Grid>
+          <Grid item sx={{ p: "2rem" }}>
+            <SaveBtn text="시작하기" />
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );

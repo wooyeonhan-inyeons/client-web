1443c7c5e6feb7c651c2c8cdb19a232b8383aa0f
lee7198 <lee7198@naver.com>
2023-06-04 12:18:23 +0900

    feat #49 개발모드 로그인처리 추가

--- src/pages/auth/LoginPage/components/SnsButton/index.tsx
+++ src/pages/auth/LoginPage/components/SnsButton/index.tsx
@@ -5,6 +5,8 @@ import { userState } from "../../../../../recoil";
 import { useRecoilState } from "recoil";
 import { createTheme, ThemeProvider } from "@mui/material";
 import { BACK_URL } from "../../../../../common";
+import { getSnsBackground } from "./style";
+
 const theme = createTheme({
   palette: {
     primary: {
@@ -12,39 +14,22 @@ const theme = createTheme({
     },
   },
 });
+
 const SnsButton = ({ sns, text, imgSrc }: SnsProps) => {
   const [, setUser] = useRecoilState(userState);
 
-  const getSnsBackground = (sns: string) => {
-    const defalutStyle = {
-      boxShadow: "0 1px 3px rgba(0,0,0,0.12)",
-      backgroundColor: "#FFFFFF",
-      color: "black",
-      width: "21rem",
-      borderRadius: "10px",
-      p: "0.6rem",
-      display: "flex",
-      justifyContent: "space-around",
-      fontSize: "1rem",
-      fontWeight: 500,
-      margin: "0.6rem",
-    };
-    if (sns === "KAKAO") {
-      defalutStyle.backgroundColor = "#F7E600";
-      defalutStyle.color = "#3A1D1D";
-      return defalutStyle;
-    }
-    return defalutStyle;
-  };
-
   const getLogin = () => {
-    if (sns === "GOOGLE") {
+    if (import.meta.env.DEV) {
       setUser((prev) => {
-        return { ...prev, first: true };
+        return {
+          ...prev,
+          first: true,
+          access_token: import.meta.env.VITE_AUTH_TOKEN,
+        };
       });
-      return;
+    } else {
+      location.replace(`${BACK_URL}/auth/${sns}`);
     }
-    location.replace(`${BACK_URL}/auth/${sns}`);
   };
 
   return (
@@ -54,11 +39,6 @@ const SnsButton = ({ sns, text, imgSrc }: SnsProps) => {
           variant="contained"
           disableElevation
           sx={getSnsBackground(sns)}
-          // onClick={() =>
-          //   setUser((prev) => {
-          //     return { ...prev, first: true };
-          //   })
-          // }
           onClick={getLogin}
         >
           <img src={imgSrc} />
